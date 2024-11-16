f7ab30a0959a1b628d5733d8496241e9c7c3a1fe
Roberto Bertelli <S7289118@studenti.unige.it>
2024-11-16 15:24:49 +0000

    Reading turtle and velocities corretly
    Signed-off-by: Roberto Bertelli <S7289118@studenti.unige.it>

--- src/assignment1_node.cpp
+++ src/assignment1_node.cpp
@@ -8,12 +8,19 @@
 #include <iostream>
 #include <cmath>
 
+#include <unistd.h>
+
 #define NEW_TURTLE "turtle_2"
 
 ros::Publisher tpub;
 
+bool check_input(int input, int size) {
+	return input > 0 and input <= size;
+}
 
 int main(int argc, char **argv) {
+	std::vector <std::string>turtles  = {"turtle_1", "turtle_2"};
+
 	ros::init(argc, argv, "turtle_subscriber");  
 	ros::NodeHandle handle;
 
@@ -25,6 +32,34 @@ int main(int argc, char **argv) {
 	t2spawn.request.name = NEW_TURTLE;
 
 	sclient.call(t2spawn);
+
+	while (true) {
+		int turtle = -1;
+
+		while (!check_input(turtle, turtles.size())) {
+			std::cout << "Insert the number of the turtle you want to control:" << std::endl;
+			for (int i = 0; i < turtles.size(); ++i) {
+			 	std::cout << i+1 << ". " << turtles[i] << std::endl;;
+			}
+			std::cout << ">> "; 
+			std::cin >> turtle;
+			if (!check_input(turtle, turtles.size())) {
+				std::cout << "Invalid turtle number, try again..." << std::endl;
+			}
+		}
+
+		float x, y, theta; 
+		std::cout << "Acquiring the velocities\n" << std::endl;
+		std::cout << "Insert the x velocity: ";
+		std::cin >> x;
+		std::cout << "Insert the y velocity: ";
+		std::cin >> y;
+		std::cout << "Insert the angular velocity: ";
+		std::cin >> theta;
+		
+		std::cout << "acquired velocities: " << x << y << theta << std::endl;
+		sleep(1);
+	}
 
 	return 0;
 }
